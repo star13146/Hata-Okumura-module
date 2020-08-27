ee181724920a81dbd7aef4794b4455ec417b1a13
star13146 <61785607+star13146@users.noreply.github.com>
2020-08-27 21:04:53 +0800

    Add files via upload

---
+++ driver.m
@@ -0,0 +1,6 @@
+hb=input('please enter the base antenna height in meters:');
+hm=input('please enter the mobile antenna height in meters:');
+f=input('please enter the frequency in million Hz:');
+flag=input('if the area is urban ,please enter 0, otherwise enter 1:\n');
+module(hb,hm,f,flag);
+
